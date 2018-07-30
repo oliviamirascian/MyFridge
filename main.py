b0e05c9a13152d985fd846a9023c9ecc36693cae
Cattien Ngo <cattien.nhu.ngo@gmail.com>
2018-07-30 15:30:31 -0700

    working on making food input work
    specifically the fridge one

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def foodInDB(response):
+    foodQuery = response.query()
+
+    foodBody = foodQuery.root.body.fetch()
+
+    if foodBody[0] == "":
+        window.alert("That food is not in our database. Check for spelling errors.")
+    else:
+        
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         welcome_template = JINJA_ENVIRONMENT.get_template('templates/welcome.html')
@@ -17,6 +27,27 @@ class FridgePage(webapp2.RequestHandler):
     def get(self):
         fridge_template = JINJA_ENVIRONMENT.get_template('templates/fridge.html')
         self.response.write(fridge_template.render())
+
+    def post(self):
+        addFood = self.request.get('addFood')
+        expirationDate = self.request.get('expirationDate')
+        removeFood = self.request.get('removeFood')
+
+        url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/autocomplete?query="+ addFood +"&number=1&metaInformation=true"
+
+        response = urlfetch.get(url,
+  headers={
+    "X-Mashape-Key": "mJg6lyimB0mshXFRCjyqO6ZJ5mUup1xzQ4ijsnldTTcG83VyNc",
+    "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+  }
+)
+
+        foodFridge = FoodFridge(name=addFood, expirationDate=expirationDate)
+
+
+        foodFridge.put()
+
+        url = foodFridge.get_url()
 
 class NutriTrackerPage(webapp2.RequestHandler):
     def get(self):
--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 from google.appengine.ext import ndb
 
-class Food(ndb.Model):
-    name = ""
-    date_entered = time.time()
-
-    
+class FoodFridge(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    #format of expiration dates: "YYYY-MM-DD" ex. "2017-06-01"
+    expirationDate = ndb.StringProperty(required=True)
