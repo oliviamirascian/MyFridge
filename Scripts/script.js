57f6366876ad9db4126d346780e1d1f8a610262b
Justin Ong <cjto2000@gmail.com>
2018-08-02 20:16:27 -0700

    morerecipestuff

--- Scripts/script.js
+++ Scripts/script.js
@@ -66,45 +66,7 @@ function removeItemFromFridge(){
 //   window.location = "http://localhost:8080";
 // }
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  window.location = "http://localhost:8080/fridge";
-}
-
-function onLoad() {
-  gapi.load('auth2', function() {
-    gapi.auth2.init();
-  });
-}
-
 // welcome.html
-
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-  window.location = "http://localhost:8080";
-}
-
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  window.location = "http://localhost:8080/fridge";
-}
-
-function onLoad() {
-  gapi.load('auth2', function() {
-    gapi.auth2.init();
-  });
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   button = document.getElementById('addFoodButton');
